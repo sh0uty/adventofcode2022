6d7945d0a9640da31899f5d71df3485533a63cc0
Luke Bocker <lukeom6@web.de>
2022-12-01 14:04:26 +0100

    Day01

---
+++ day01/solve.py
@@ -0,0 +1,19 @@
+with open('./data1.txt') as file:
+    data = [x.strip() for x in file.readlines()]
+
+calories = 0
+sums = []
+
+for entry in data:
+    if not entry:
+        sums.append(calories)
+        calories = 0
+    else:
+        calories += int(entry)
+         
+
+print(f"Part 1: {max(sums)}")
+
+sums = sorted(sums)
+
+print(f"Part 2: {sum(sums[-3:])}")
