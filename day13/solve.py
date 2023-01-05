47fbb774431bb2bf4366423db0299e437abde0c7
Luke Bocker <lukeom6@web.de>
2023-01-05 14:15:42 +0100

    Day13 Part 1

---
+++ day13/solve.py
@@ -0,0 +1,44 @@
+import sys
+
+with open('day13/input.txt', 'r') as file:
+    data = [x.split('\n') for x in file.read().split('\n\n')]
+
+def compare(t1, t2):
+    if isinstance(t1, list) and isinstance(t2, list):
+        for entry in zip(t1, t2):
+            result = compare(entry[0], entry[1])
+            if result == 0:
+                continue
+            else:
+                return result
+        if len(t1) > len(t2):
+            return 1
+        elif len(t2) > len(t1):
+            return -1
+        else:
+            return 0
+
+    if isinstance(t1, int) and isinstance(t2, int):
+        if t1 > t2:
+            return 1
+        elif t1 < t2:
+            return -1
+        else:
+            return 0
+
+    if isinstance(t1, list):
+        t2 = [t2]
+    elif isinstance(t2, list):
+        t1 = [t1]
+
+    return compare(t1, t2)
+
+    
+total = 0
+
+for index, entry in enumerate(data):
+    val = compare(eval(entry[0]), eval(entry[1]))
+    if val == -1:
+        total += index + 1
+
+print(f'Part 1: {total}')
